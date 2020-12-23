7244f17c0a744a20a04cb78698e3f8cfdaae530f
harshdhamecha <62664549+harshdhamecha@users.noreply.github.com>
2020-12-23 19:26:12 +0530

    Add files via upload

---
+++ Dice-Game/dice_game.py
@@ -0,0 +1,91 @@
+import random as rd
+import time
+
+def AllMaximum(scores):
+  maxi = max(scores)
+  return [index for index, item in enumerate(scores) if item == maxi]
+
+def HandleEquals(scores, players, points):
+  if len(set(scores)) == 1:
+    print('\nAll the players have scored the same points in this round!')
+    for index in range(len(points)):
+      points[index] += 1
+  else:
+    indices = AllMaximum(scores)
+    if len(indices) < 2:
+      indices = ''.join(str(i) for i in indices)
+      print(f'\n{players[int(indices)]} has won in this round!')
+      points[int(indices)] += 1
+    else:
+      print()
+      for index in indices:
+        points[index] += 1
+        print(players[index], end=', ')
+      print('--> scored the same points in this round!')
+
+def RoundResult(scores, players, points):
+  if len(scores) != len(set(scores)):
+    HandleEquals(scores, players, points)
+  else:
+    index = scores.index(max(scores))
+    points[index] += 1
+    print(f'\n{players[index]} has won this round!')
+
+def GameResult(points, players):
+  if len(set(points)) == 1:
+    print(f'\n\nAll the players have scored the same points')
+    print('Congatulations! All')
+  else:
+    indices = AllMaximum(points)
+    if len(indices) < 2:
+      indices = ''.join(str(i) for i in indices)
+      print(f'\n{players[int(indices)]} has won the game!')
+      print('Congratulations!', players[int(indices)])
+    else:
+      print()
+      for index in indices:
+        print(players[index], end=', ')
+      print('--> scored the same points in the game!')
+
+def DiceGame(rounds, n_players, players):
+  points = [0 for _ in range(n_players)]
+  for round in range(1, rounds + 1):
+    scores = []
+    print('\n\nRound:', round)
+    for player in players:
+      time.sleep(0.4)
+      print(f"It's your turn {player}")
+      time.sleep(0.2)
+      temp = input('Press enter to roll a dice: ')
+      score = rd.randint(1, 6)
+      time.sleep(0.5)
+      print(f'Your score is: {score}')
+      scores.append(score)
+    RoundResult(scores, players, points)
+  GameResult(points, players)
+ 
+def GameRepeat():
+  time.sleep(0.5)
+  decision = input('\n\nWould you like to play the game once again(y/n)? ').lower()
+  if decision == 'y':
+    main()
+  elif decision != 'y' and decision != 'n':
+    print('You\'ve wrongly pressed key other than \'y\' or \'n\'')
+    GameRepeat()
+  else:
+    return
+
+# Driver Code
+def main():
+  rounds = int(input('\nEnter the total number of rounds you want to play for: '))
+  n_players = int(input('Enter the total number of players: '))
+  players = []
+
+  for player in range(1, n_players+1):
+    players.append(input(f'Enter the name of player{player}: '))
+
+  DiceGame(rounds, n_players, players)
+  GameRepeat()
+
+print('-------------Dice Game---------------')
+main()
---
+++ Dice-Simulator/dice_simulator.py
@@ -0,0 +1,52 @@
+import random as rd
+import time
+
+def DiceRoll(attempts):
+  scores = []
+  for attempt in range(attempts):
+    print('\nAttempts Remaining:', attempts - attempt)
+    time.sleep(0.5)
+    temp = input('Press enter to roll a dice: ')
+    time.sleep(0.7)
+    score = rd.randint(1, 6)
+    print('You got', score, 'on a dice')
+    scores.append(score)
+  return scores
+
+def try_more():
+  time.sleep(1)
+  decision = input('\nWould you like to roll a dice once again (y/n)? ').lower()
+  if decision == 'y':
+    time.sleep(0.5)
+    temp = input('Press enter to roll a dice: ')
+    time.sleep(0.7)
+    score = rd.randint(1, 6)
+    print('You got', score, 'on a dice')
+    scores.append(score)
+    try_more()
+  elif decision != 'y' and decision != 'n':
+    print('You\'ve wrongly pressed key other than \'y\' or \'n\'')
+    try_more()
+  else:
+    return 
+
+def print_scores(scores):
+  for score in range(1, len(scores)+1):
+    print(f'Attempt {score}: {scores[score-1]}')
+
+# Driver Code
+attempts = int(input('How many times would you like to roll a dice? '))
+scores = DiceRoll(attempts)
+
+if 6 not in scores:
+  time.sleep(0.5)
+  print('\nYou haven\'t got the highest number yet!')
+  try_more()
+else:
+  time.sleep(0.5)
+  print('\nYou\'ve got 6, Great going buddy!')
+  try_more()
+
+print('\nFollowing is your attempt-wise scores:')  
+print_scores(scores)
+print('\nYou scored total', sum(scores), 'points in', len(scores), 'attempts')
